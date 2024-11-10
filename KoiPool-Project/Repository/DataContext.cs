96c3ea20b09cf6da37d33d82b2c87ca86af6feb2
VuTriDung <dungvutri2k5@gmail.com>
2024-11-10 20:24:11 +0700

    Sửa database

--- KoiPool-Project/Repository/DataContext.cs
+++ KoiPool-Project/Repository/DataContext.cs
@@ -13,11 +13,7 @@ public class DataContext : IdentityDbContext<AppUserModel>
 
     public DbSet<AppUserModel> AppUsers { get; set; }
     public DbSet<HistoryModel> Histories { get; set; }
-    public DbSet<RequestDetailModel> RequestDetails { get; set; }
     public DbSet<RequestModel> Requests { get; set; }
-    public DbSet<ServiceModel> Services { get; set; }
-    public DbSet<EmployeeModel> Employees { get; set; }
-    public DbSet<CustomerModel> Customers { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
